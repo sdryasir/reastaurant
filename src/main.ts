70270569d05dbb1f3e3b3d03d7dcde646924661e
Yasir Ali <sdr.yasir@gmail.com>
2025-08-30 04:19:05 -0700

    configured swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+
+
+  const config = new DocumentBuilder()
+              .setTitle("Restaurant API")
+              .setDescription('This is a restaurant API')
+              .setVersion('1.0')
+              .build();
+
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document)
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
